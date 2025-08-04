3af88ce7b9ac2aca8cc78c10d13299b93e85c446
DarthMooMancer <dmm.dev@pm.me>
2025-08-04 14:15:26 -0400

    wip

--- .config/suckless/slstatus/config.mk
+++
@@ -1,8 +0,0 @@
-PREFIX = /usr/local
-
-CPPFLAGS = $(shell pkg-config --cflags x11) -D_DEFAULT_SOURCE
-LDFLAGS  = $(shell pkg-config --libs-only-L x11) -s
-LDLIBS   = $(shell pkg-config --libs-only-l x11)
-CFLAGS = -std=c17 -pedantic -Wall -Wextra -Wno-unused-parameter -O2 -march=native
-
-CC = gcc
