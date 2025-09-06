dbee1fa86fb9cd65e29ee6e8066686bc393e7577
DarthMooMancer <dmm.dev@pm.me>
2025-09-06 18:50:44 -0400

    nvim: revert last

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -32,7 +32,6 @@ require("lazy").setup({
 		{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
 			config = function()
 				require("nvim-treesitter.configs").setup({
-					auto_install = true,
 					highlight = { enable = true },
 					indent = { enable = true }
 				})
