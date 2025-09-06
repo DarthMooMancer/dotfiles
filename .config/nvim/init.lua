1c46b182fcfd5e35ed729f46deaaf2082d1f8487
DarthMooMancer <dmm.dev@pm.me>
2025-09-06 18:47:33 -0400

    nvim: removing random plugin and adding auto_install to treesitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
 		{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
 			config = function()
 				require("nvim-treesitter.configs").setup({
+					auto_install = true,
 					highlight = { enable = true },
 					indent = { enable = true }
 				})
